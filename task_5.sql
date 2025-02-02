ebbd109327aa9310a07d7ebc2280280627857cb6
Olakhaled55 <olakhaled611@gmail.com>
2025-02-02 14:51:49 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,4 @@
--- Insert data into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES
-    (1, 'John Doe', 'johndoe@example.com', '123 Elm Street'),
-    (2, 'Jane Smith', 'janesmith@example.com', '456 Oak Avenue'),
-    (3, 'Alice Johnson', 'alicej@example.com', '789 Pine Road');
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
