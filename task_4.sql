0bf48e54ec69ad6bb805cfa062eb5965287d4956
Olakhaled55 <olakhaled611@gmail.com>
2025-02-04 13:04:49 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,17 @@
-USE INFORMATION_SCHEMA;
+-- This script prints the full description of the table `books` from the specified database
+-- without using the DESCRIBE or EXPLAIN statements.
+
+USE alx_book_store;
 
 SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_DEFAULT, 
-    EXTRA 
+    COLUMN_NAME AS 'Field', 
+    COLUMN_TYPE AS 'Type', 
+    IS_NULLABLE AS 'Null', 
+    COLUMN_KEY AS 'Key', 
+    COLUMN_DEFAULT AS 'Default', 
+    EXTRA AS 'Extra'
 FROM 
-    COLUMNS 
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'books';
+    TABLE_SCHEMA = DATABASE() AND 
+    TABLE_NAME = 'books';
